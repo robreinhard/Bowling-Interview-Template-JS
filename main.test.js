7fd119d294cbd36f85014ea138b0138a8ca108b2
Rob Reinhard <rreinhard@macbook-pro-4.mynetworksettings.com>
2023-11-01 16:04:53 -0500

    Two tests, for clarity

--- main.test.js
+++ main.test.js
@@ -2,7 +2,20 @@ import {expect, test} from "@jest/globals";
 
 import Game from "./Game.js";
 
-test('Rolling all strikes, then a 9', () => {
+test('Roll strikes 1st-9th inning, then a 0 followed by a 9', () => {
+    const game = new Game();
+
+    for (let inning = 1; inning <= 9; inning++) {
+        game.roll(10);
+    }
+
+    game.roll(0);
+    game.roll(9);
+
+    expect(game.score()).toEqual(258);
+})
+
+test('Roll strikes 1st-9th inning, then a 9 followed by a 0', () => {
     const game = new Game();
 
     for (let inning = 1; inning <= 9; inning++) {
@@ -10,6 +23,7 @@ test('Rolling all strikes, then a 9', () => {
     }
 
     game.roll(9);
+    game.roll(0);
 
-    expect(game.score()).toEqual(258);
+    expect(game.score()).toEqual(267);
 })
